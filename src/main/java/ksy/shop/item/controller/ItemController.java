bb4d6363146d71327d3692e5f3f7ac0ee3b3bdab
김승연 <tmddus3002@gmail.com>
2024-09-07 22:01:14 +0900

    commit

--- src/main/java/ksy/shop/item/controller/ItemController.java
+++ src/main/java/ksy/shop/item/controller/ItemController.java
@@ -28,6 +28,8 @@ public class ItemController {
         //페이징 처리
         Map<String,String> map = PagingUtil.paging(currentPage,itemNum);
         List<ItemVO> itemList = itemService.getItemList(map);
+        //다음 페이지를 어떻게 처리할건지 생각해봐야함
+        //기존에 페이징 처리를 html로 작성해서 반환하던 PagingUtil이 아니기 때문에 어떤식으로 반환할건지 생각해보자
 
         model.addAttribute("itemList", itemList);
         return "/item/list";
