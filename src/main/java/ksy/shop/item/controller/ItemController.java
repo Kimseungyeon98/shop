945090c13efb6b1f0bb57a1c69215b681867b65a
김승연 <tmddus3002@gmail.com>
2024-10-01 00:59:02 +0900

    파일 업로드

--- src/main/java/ksy/shop/item/controller/ItemController.java
+++ src/main/java/ksy/shop/item/controller/ItemController.java
@@ -9,15 +9,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @Slf4j
@@ -48,19 +52,41 @@ public class ItemController {
         return "/item/register";
     }
     @PostMapping("/item/register")
-    public String register(HttpSession session, ItemVO item, @RequestParam MultipartFile file){
+    public String register(HttpSession session, ItemVO item, @RequestParam MultipartFile file) throws IOException {
         MemberVO user = (MemberVO)session.getAttribute("user");
         if(user==null){
             return "/member/login";
         } else {
             item.setMember(user);
         }
-        item.setImage(file.getOriginalFilename());
-        // 파일 업로드 경로 설정
-        // 파일 업로드 로직
+        if(!file.isEmpty()){
+            // 파일 업로드 로직
+            //1. 원본 파일 이름 가져오기
+            String originalFilename = file.getOriginalFilename();
+            //2. 확장자 앞 까지 파일 이름 인덱스 찾기
+            int pos = originalFilename.lastIndexOf(".");
+            //3. 확장자 저장
+            String ext = originalFilename.substring(pos+1);
+            //4. uuid 코드 생성
+            String uuid = UUID.randomUUID().toString();
+            //5. uuid 코드 + 확장자
+            String storeFileName = uuid + "." + ext;
+
+            // 파일 업로드 경로 설정
+            String uploadDir = "src/main/resources/static/upload/";
+            File uploadPath = new File(uploadDir);
+            if (!uploadPath.exists()) {
+                uploadPath.mkdirs();  // 디렉토리 생성
+            }
+            
+            //File.separator 는 운영체제마다 다른 구분자를 자동 지정해줌
+            file.transferTo(new File(uploadPath.getAbsolutePath() + File.separator + storeFileName));
+
+            item.setImage(storeFileName);
+        }
 
         itemService.registerItem(item);
-        return "redirect:/item/list";
+        return "redirect:/item";
     }
 
     @GetMapping("/item/detail/{itemNum}")
